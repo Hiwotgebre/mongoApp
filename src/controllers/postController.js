b753812994eb5fff66cb731a384fbc032d767e9e
Hiwotgebre <hiwotg173@gmail.com>
2024-06-27 22:55:29 -0400

    Create controllers for posts

---
+++ src/controllers/postController.js
@@ -0,0 +1,60 @@
+const Post = require('../models/post');
+
+// Create a new post
+exports.createPost = async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(201).send(newPost);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Get all posts
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).send(posts);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Get a single post by ID
+exports.getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).send();
+    }
+    res.status(200).send(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Update a post
+exports.updatePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!post) {
+      return res.status(404).send();
+    }
+    res.status(200).send(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Delete a post
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) {
+      return res.status(404).send();
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
