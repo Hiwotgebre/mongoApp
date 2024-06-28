cfcdde30545ef26c9f5c0d3608cd3a7a3b7c9b6e
Hiwotgebre <hiwotg173@gmail.com>
2024-06-27 23:34:28 -0400

    Adding indexes in to post.js for title for faster search

--- src/models/post.js
+++ src/models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, index: true }, //Indexed for faster search
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now }
